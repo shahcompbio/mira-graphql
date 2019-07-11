18d3127c02acaef5ff6dc827162bb115007c35df
Charumathi Badrinath <charumathi.badrinath@gmail.com>
2019-07-11 16:05:26 -0400

    Implemented GraphQL querying from Elasticsearch database

--- src/celltypes.js
+++ src/celltypes.js
@@ -1,12 +1,13 @@
 const { gql } = require("apollo-server");
-import { data } from "./data.js";
+import bodybuilder from "bodybuilder";
+import client from "./api/elasticsearch.js";
 
 export const schema = gql`
   extend type Query {
-    markers: [dataPair!]!
+    cellAndMarkerGenesPair: [Rho!]!
   }
 
-  type dataPair {
+  type Rho {
     cellType: String!
     markerGenes: [String!]!
   }
@@ -14,8 +15,25 @@ export const schema = gql`
 
 export const resolvers = {
   Query: {
-    async markers() {
-      return data;
+    async cellAndMarkerGenesPair() {
+      const query = bodybuilder()
+        .size(0)
+        .aggregation("terms", "celltype", { size: 100 }, a => {
+          return a.aggregation("terms", "marker_gene", { size: 100 });
+        })
+        .build();
+
+      const results = await client.search({
+        index: "patient_09443_e_rho",
+        body: query
+      });
+
+      return results["aggregations"]["agg_terms_celltype"]["buckets"];
     }
+  },
+  Rho: {
+    cellType: root => root.key,
+    markerGenes: root =>
+      root["agg_terms_marker_gene"]["buckets"].map(marker => marker.key)
   }
 };
