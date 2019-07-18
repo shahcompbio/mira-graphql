4c7046347748bd50021fec1860da935a250c34f1
Charumathi Badrinath <charumathi.badrinath@gmail.com>
2019-07-18 14:55:39 -0400

    Added ability to always query existing cell types in sample + expanded queries to encompass all loaded patients

--- src/celltypes.js
+++ src/celltypes.js
@@ -4,7 +4,8 @@ import client from "./api/elasticsearch.js";
 
 export const schema = gql`
   extend type Query {
-    cellAndMarkerGenesPair: [Rho!]!
+    cellAndMarkerGenesPair(patientID: String!): [Rho!]!
+    existingCellTypes(patientID: String!, sampleID: String!): [String!]!
   }
 
   type Rho {
@@ -15,20 +16,38 @@ export const schema = gql`
 
 export const resolvers = {
   Query: {
-    async cellAndMarkerGenesPair() {
+    async cellAndMarkerGenesPair(_, { patientID }) {
       const query = bodybuilder()
         .size(0)
+        .filter("term", "patient_id", patientID)
         .aggregation("terms", "celltype", { size: 100 }, a => {
           return a.aggregation("terms", "marker_gene", { size: 100 });
         })
         .build();
 
       const results = await client.search({
-        index: "patient_09443_e_rho",
+        index: `${patientID.toLowerCase()}_rho`,
         body: query
       });
 
       return results["aggregations"]["agg_terms_celltype"]["buckets"];
+    },
+    async existingCellTypes(_, { patientID, sampleID }) {
+      const query = bodybuilder()
+        .size(0)
+        .filter("term", "patient_id", patientID)
+        .filter("term", "sample_id", sampleID)
+        .aggregation("terms", "cell_type", { size: 100 })
+        .build();
+
+      const results = await client.search({
+        index: `${patientID.toLowerCase()}_cells`,
+        body: query
+      });
+
+      return results["aggregations"]["agg_terms_cell_type"]["buckets"]
+        .map(element => element.key)
+        .sort();
     }
   },
   Rho: {
