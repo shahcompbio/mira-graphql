2d6e0fe8a34ff86d74a751cebb1884066d256e6d
redpanda-cat <leungs1@mskcc.org>
2019-09-26 12:44:40 -0400

    Change HOST config

--- src/api/elasticsearch.js
+++ src/api/elasticsearch.js
@@ -1,8 +1,8 @@
 const elasticsearch = require("elasticsearch");
 
-const HOST = process.env.HOST || "localhost";
+const HOST = process.env.HOST || "localhost:9200";
 const client = new elasticsearch.Client({
-  host: HOST + ":" + "9200"
+  host: HOST
 });
 
 export default client;
