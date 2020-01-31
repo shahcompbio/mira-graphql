3ab5649a3f7ceadfc895fa4ce5f0f6975061c29c
redpanda-cat <leungs1@mskcc.org>
2020-01-31 14:16:21 -0500

    Refactor and clean up density calculations

--- src/density.js
+++ src/density.js
@@ -5,69 +5,24 @@ import client from "./api/elasticsearch.js";
 
 export const schema = gql`
   extend type Query {
-    density(type: String!, dashboardID: String!): [Bins!]
-    density2(type: String!, dashboardID: String!): [DensityBin!]
-    density3(
-      type: String!
+    density(
       dashboardID: String!
+      label: DashboardAttributeInput!
       highlightedGroup: String
-    ): [DensityBin2!]
-  }
-  type Bins {
-    size: Int!
-    bin: [DensityBin!]
+    ): [DensityBin!]
   }
 
   type DensityBin {
     x: Float!
     y: Float!
-    values: [DensityBinValue!]
-  }
-
-  type DensityBinValue {
-    celltype: String!
-    proportion: Float!
-    count: Int!
-  }
-
-  type DensityBin2 {
-    x: Float!
-    y: Float!
-    values: [DensityBinValue2!]
-  }
-
-  type DensityBinValue2 {
     label: String!
-    value: Float!
+    value: StringOrNum!
   }
 `;
 
 export const resolvers = {
   Query: {
-    async density(_, { type, dashboardID }) {
-      const query = bodybuilder()
-        .size(50000)
-        .filter("term", "dashboard_id", dashboardID)
-        .aggregation("terms", "bin_size", {
-          size: 50000,
-          order: { _key: "asc" }
-        })
-        .build();
-
-      const results = await client.search({
-        index: `dashboard_cells_density`,
-        body: query
-      });
-      const records = results["aggregations"]["agg_terms_bin_size"][
-        "buckets"
-      ].map(bucket => ({
-        binSize: bucket["key"],
-        dashboardID
-      }));
-      return records;
-    },
-
-    async density2(_, { type, dashboardID }) {
+    async density(_, { dashboardID, highlightedGroup, label }) {
       const sizeQuery = bodybuilder()
         .size(0)
         .filter("term", "dashboard_id", dashboardID)
@@ -84,198 +39,202 @@ export const resolvers = {
 
       const xBinSize = (agg_stats_x["max"] - agg_stats_x["min"]) / 100;
       const yBinSize = (agg_stats_y["max"] - agg_stats_y["min"]) / 100;
-
-      const query = bodybuilder()
-        .size(0)
-        .filter("term", "dashboard_id", dashboardID)
-
-        .aggregation(
-          "histogram",
-          "x",
-          { interval: xBinSize, min_doc_count: 1 },
-          a =>
-            a.aggregation(
-              "histogram",
-              "y",
-              { interval: yBinSize, min_doc_count: 1 },
-              a => a.aggregation("terms", "cell_type", { size: 1000 })
-            )
-        )
-        .build();
-
-      const results = await client.search({
-        index: "dashboard_cells",
-        body: query
-      });
-
-      const records = results["aggregations"]["agg_histogram_x"][
-        "buckets"
-      ].reduce(
-        (records, xBucket) => [...records, ...processXBuckets(xBucket)],
-        []
+      const data = await getBinnedData(
+        dashboardID,
+        label,
+        highlightedGroup,
+        xBinSize,
+        yBinSize
       );
-
-      return records;
-    },
-
-    async density3(_, { type, dashboardID, highlightedGroup }) {
-      const sizeQuery = bodybuilder()
-        .size(0)
-        .filter("term", "dashboard_id", dashboardID)
-        .aggregation("stats", "x")
-        .aggregation("stats", "y")
-        .build();
-
-      const sizeResults = await client.search({
-        index: "dashboard_cells",
-        body: sizeQuery
-      });
-
-      const { agg_stats_x, agg_stats_y } = sizeResults["aggregations"];
-
-      const xBinSize = (agg_stats_x["max"] - agg_stats_x["min"]) / 100;
-      const yBinSize = (agg_stats_y["max"] - agg_stats_y["min"]) / 100;
-
-      const query = bodybuilder()
-        .size(0)
-        .filter("term", "dashboard_id", dashboardID)
-
-        .aggregation(
-          "histogram",
-          "x",
-          { interval: xBinSize, min_doc_count: 1 },
-          a =>
-            a.aggregation(
-              "histogram",
-              "y",
-              { interval: yBinSize, min_doc_count: 1 },
-              a => a.aggregation("terms", "cell_type", { size: 1000 })
-            )
-        )
-        .build();
-
-      const results = await client.search({
-        index: "dashboard_cells",
-        body: query
-      });
-
-      const records = results["aggregations"]["agg_histogram_x"][
-        "buckets"
-      ].reduce(
-        (records, xBucket) => [
-          ...records,
-          ...processXBuckets2(xBucket, highlightedGroup)
-        ],
-        []
-      );
-
-      return records;
+      return data;
     }
-  },
-
-  Bins: {
-    size: root => root["binSize"],
-    bin: async ({ binSize, dashboardID }) => {
-      const query = bodybuilder()
-        .size(50000)
-        .filter("term", "dashboard_id", dashboardID)
-        .filter("term", "bin_size", binSize)
-        .sort([{ x: "asc" }, { y: "asc" }, { count: "desc" }])
-        .build();
-
-      const results = await client.search({
-        index: `dashboard_cells_density`,
-        body: query
-      });
-      const records = condenseResults(
-        results["hits"]["hits"].map(record => record["_source"])
-      );
-      return records;
-    }
-  },
-
-  DensityBin: {
-    x: root => root["x"],
-    y: root => root["y"],
-    values: root => {
-      const total = root["values"].reduce(
-        (total, record) => total + record["count"],
-        0
-      );
-
-      return root["values"].map(record => ({ ...record, total }));
-    }
-  },
-
-  DensityBin2: {
-    x: root => root["x"],
-    y: root => root["y"],
-    values: root => {
-      const total = root["values"].reduce(
-        (total, record) => total + record["count"],
-        0
-      );
-
-      return root["values"].map(record => ({
-        ...record,
-        total,
-        highlightedGroup: root["highlightedGroup"]
-      }));
-    }
-  },
-
-  DensityBinValue: {
-    celltype: root => root["celltype"],
-    proportion: root => root["count"] / root["total"],
-    count: root => root["count"]
-  },
-
-  DensityBinValue2: {
-    label: root => root["celltype"],
-    value: root =>
-      !root["highlightedGroup"] ? root["count"] : root["count"] / root["total"]
   }
 };
 
-const condenseResults = records => {
-  const helper = (records, rsf, currXY, acc) => {
-    if (records.length === 0) {
-      return [...rsf, { ...currXY, values: acc }];
-    } else {
-      const [currRecord, ...restRecords] = records;
-      if (currXY["x"] === currRecord["x"] && currXY["y"] === currRecord["y"]) {
-        return helper(restRecords, rsf, currXY, [...acc, currRecord]);
-      } else {
-        return helper(
-          restRecords,
-          [...rsf, { ...currXY, values: acc }],
-          currRecord,
-          [currRecord]
-        );
-      }
-    }
-  };
-  const [firstRecord, ...restRecords] = records;
-  return helper(restRecords, [], firstRecord, [firstRecord]);
+async function getBinnedData(
+  dashboardID,
+  label,
+  highlightedGroup,
+  xBinSize,
+  yBinSize
+) {
+  if (label["label"] === "celltype") {
+    const query = getBaseDensityQuery(dashboardID, xBinSize, yBinSize, a =>
+      a.aggregation("terms", "cell_type", { size: 1000 })
+    );
+
+    const results = await client.search({
+      index: "dashboard_cells",
+      body: query.build()
+    });
+
+    const records = results["aggregations"]["agg_histogram_x"][
+      "buckets"
+    ].reduce(
+      (records, xBucket) => [
+        ...records,
+        ...processXBuckets(
+          xBucket,
+          xBinSize,
+          yBinSize,
+          !highlightedGroup ? label["label"] : highlightedGroup,
+          !highlightedGroup
+            ? yBucket => yBucket["agg_terms_cell_type"]["buckets"][0]["key"]
+            : yBucket =>
+                calculateProportion(
+                  yBucket["agg_terms_cell_type"]["buckets"],
+                  highlightedGroup
+                )
+        )
+      ],
+      []
+    );
+
+    return records;
+  } else if (label["type"] === "CELL") {
+    const query = getBaseDensityQuery(dashboardID, xBinSize, yBinSize, a =>
+      a.aggregation("stats", label["label"])
+    );
+
+    const results = await client.search({
+      index: "dashboard_cells",
+      body: query.build()
+    });
+
+    const filteredBins = await getFilteredBins(
+      highlightedGroup,
+      dashboardID,
+      xBinSize,
+      yBinSize,
+      label["label"]
+    );
+
+    const records = results["aggregations"]["agg_histogram_x"][
+      "buckets"
+    ].reduce(
+      (records, xBucket) => [
+        ...records,
+        ...processXBuckets(
+          xBucket,
+          xBinSize,
+          yBinSize,
+          label["label"],
+          yBucket => {
+            const xEncoded = Math.round(xBucket["key"] / xBinSize);
+            const yEncoded = Math.round(yBucket["key"] / yBinSize);
+
+            return filteredBins.hasOwnProperty(xEncoded) &&
+              filteredBins[xEncoded].hasOwnProperty(yEncoded)
+              ? filteredBins[xEncoded][yEncoded]
+              : !highlightedGroup
+              ? yBucket[`agg_stats_${label["label"]}`]["avg"]
+              : 0;
+          }
+        )
+      ],
+      []
+    );
+
+    return records;
+  } else if (label["type"] === "SAMPLE") {
+  } else {
+  }
+}
+
+const getBaseDensityQuery = (dashboardID, xBinSize, yBinSize, labelAgg) =>
+  bodybuilder()
+    .size(0)
+    .filter("term", "dashboard_id", dashboardID)
+    .aggregation(
+      "histogram",
+      "x",
+      { interval: xBinSize, min_doc_count: 1 },
+      a =>
+        a.aggregation(
+          "histogram",
+          "y",
+          { interval: yBinSize, min_doc_count: 1 },
+          labelAgg
+        )
+    );
+
+const processXBuckets = (xBucket, xBinSize, yBinSize, label, getValue) =>
+  xBucket["agg_histogram_y"]["buckets"].map(yBucket => {
+    return {
+      x: Math.round(xBucket["key"] / xBinSize),
+      y: Math.round(yBucket["key"] / yBinSize),
+      value: getValue(yBucket),
+      label
+    };
+  });
+
+const calculateProportion = (counts, highlightedGroup) => {
+  const total = counts.reduce(
+    (currSum, record) => currSum + record["doc_count"],
+    0
+  );
+
+  const filteredRecords = counts.filter(
+    record => record["key"] === highlightedGroup
+  );
+
+  return filteredRecords.length === 0
+    ? 0
+    : filteredRecords[0]["doc_count"] / total;
 };
 
-const processXBuckets = xBucket =>
-  xBucket["agg_histogram_y"]["buckets"].map(yBucket => ({
-    x: xBucket["key"],
-    y: yBucket["key"],
-    values: yBucket["agg_terms_cell_type"]["buckets"].map(celltypeBucket => ({
-      celltype: celltypeBucket["key"],
-      count: celltypeBucket["doc_count"]
-    }))
-  }));
+async function getFilteredBins(
+  highlightedGroup,
+  dashboardID,
+  xBinSize,
+  yBinSize,
+  label
+) {
+  if (highlightedGroup) {
+    const filteredQuery = bodybuilder()
+      .size(0)
+      .filter("term", "dashboard_id", dashboardID)
+      .filter("term", "cell_type", highlightedGroup)
+      .aggregation(
+        "histogram",
+        "x",
+        { interval: xBinSize, min_doc_count: 1 },
+        a =>
+          a.aggregation(
+            "histogram",
+            "y",
+            { interval: yBinSize, min_doc_count: 1 },
+            a => a.aggregation("stats", label)
+          )
+      )
+      .build();
 
-const processXBuckets2 = (xBucket, highlightedGroup) =>
-  xBucket["agg_histogram_y"]["buckets"].map(yBucket => ({
-    x: xBucket["key"],
-    y: yBucket["key"],
-    highlightedGroup,
-    values: yBucket["agg_terms_cell_type"]["buckets"].map(celltypeBucket => ({
-      celltype: celltypeBucket["key"],
-      count: celltypeBucket["doc_count"]
-    }))
-  }));
+    const filteredResults = await client.search({
+      index: "dashboard_cells",
+      body: filteredQuery
+    });
+
+    const processProbYBucket = yBuckets =>
+      yBuckets.reduce(
+        (yMap, bucket) => ({
+          ...yMap,
+          [Math.round(bucket["key"] / yBinSize)]: bucket[`agg_stats_${label}`][
+            "avg"
+          ]
+        }),
+        {}
+      );
+
+    return filteredResults["aggregations"]["agg_histogram_x"]["buckets"].reduce(
+      (xMap, bucket) => ({
+        ...xMap,
+        [Math.round(bucket["key"] / xBinSize)]: processProbYBucket(
+          bucket["agg_histogram_y"]["buckets"]
+        )
+      }),
+      {}
+    );
+  } else {
+    return {};
+  }
+}
