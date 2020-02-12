3a717dd662121f84403da5bf83113d68ef13bf22
redpanda-cat <leungs1@mskcc.org>
2020-02-12 12:42:58 -0500

    Filled in cumulative genes resolver

--- src/cumulative-genes.js
+++ src/cumulative-genes.js
@@ -12,7 +12,130 @@ export const schema = gql`
 export const resolvers = {
   Query: {
     async cumulativeGenes(_, { dashboardID, genes }) {
-      return [];
+      const sizeQuery = bodybuilder()
+        .size(0)
+        .filter("term", "dashboard_id", dashboardID)
+        .aggregation("stats", "x")
+        .aggregation("stats", "y")
+        .build();
+
+      const sizeResults = await client.search({
+        index: "dashboard_cells",
+        body: sizeQuery
+      });
+
+      const { agg_stats_x, agg_stats_y } = sizeResults["aggregations"];
+
+      const xBinSize = (agg_stats_x["max"] - agg_stats_x["min"]) / 100;
+      const yBinSize = (agg_stats_y["max"] - agg_stats_y["min"]) / 100;
+
+      const getDataBins = (results, getValue) =>
+        results["aggregations"]["agg_histogram_x"]["buckets"].reduce(
+          (xMap, xBucket) => ({
+            ...xMap,
+            [Math.round(xBucket["key"] / xBinSize)]: processProbYBucket(
+              xBucket["agg_histogram_y"]["buckets"],
+              getValue
+            )
+          }),
+          {}
+        );
+
+      const processProbYBucket = (yBuckets, getValue) =>
+        yBuckets.reduce(
+          (yMap, bucket) => ({
+            ...yMap,
+            [Math.round(bucket["key"] / yBinSize)]: getValue(bucket)
+          }),
+          {}
+        );
+      const query = bodybuilder()
+        .size(0)
+        .filter("terms", "gene", genes)
+        .aggregation(
+          "histogram",
+          "x",
+          { interval: xBinSize, min_doc_count: 1 },
+          a =>
+            a.aggregation(
+              "histogram",
+              "y",
+              { interval: yBinSize, min_doc_count: 1 },
+              a => a.aggregation("stats", "log_count")
+            )
+        )
+        .build();
+
+      const results = await client.search({
+        index: `dashboard_genes_${dashboardID.toLowerCase()}`,
+        body: query
+      });
+
+      const allBins = await getAllBins(dashboardID, xBinSize, yBinSize);
+
+      const dataBins = getDataBins(
+        results,
+        yBucket => yBucket["agg_stats_log_count"]["sum"]
+      );
+      return allBins.map(record => ({
+        ...record,
+        label: "",
+        value:
+          dataBins.hasOwnProperty(record["x"]) &&
+          dataBins[record["x"]].hasOwnProperty(record["y"])
+            ? dataBins[record["x"]][record["y"]]
+            : 0
+      }));
     }
   }
 };
+async function getAllBins(dashboardID, xBinSize, yBinSize) {
+  const query = getBaseDensityQuery(dashboardID, xBinSize, yBinSize, a =>
+    a.aggregation("terms", "cell_type", { size: 1000 })
+  );
+  const results = await client.search({
+    index: "dashboard_cells",
+    body: query.build()
+  });
+
+  return results["aggregations"]["agg_histogram_x"]["buckets"].reduce(
+    (records, xBucket) => [
+      ...records,
+      ...processXBuckets(
+        xBucket,
+        xBinSize,
+        yBinSize,
+        "total",
+        yBucket => yBucket["doc_count"]
+      )
+    ],
+    []
+  );
+}
+
+const getBaseDensityQuery = (dashboardID, xBinSize, yBinSize, labelAgg) =>
+  bodybuilder()
+    .size(0)
+    .filter("term", "dashboard_id", dashboardID)
+    .aggregation(
+      "histogram",
+      "x",
+      { interval: xBinSize, min_doc_count: 1 },
+      a =>
+        a.aggregation(
+          "histogram",
+          "y",
+          { interval: yBinSize, min_doc_count: 1 },
+          labelAgg
+        )
+    );
+
+const processXBuckets = (xBucket, xBinSize, yBinSize, label, getValue) =>
+  xBucket["agg_histogram_y"]["buckets"].map(yBucket => {
+    return {
+      x: Math.round(xBucket["key"] / xBinSize),
+      y: Math.round(yBucket["key"] / yBinSize),
+      value: getValue(yBucket),
+      label
+    };
+  });
