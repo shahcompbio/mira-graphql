d62b9f8ba5d6bb0197325908e8a91bf6f9a51554
redpanda-cat <leungs1@mskcc.org>
2020-02-24 17:30:39 -0500

    Revert "minor: remove console log, merge with prev"
    This reverts commit 1b5cb08257a4b69fa74c24c842f602bd6a5aba9a.

--- src/dashboard.js
+++ src/dashboard.js
@@ -179,6 +179,7 @@ export const resolvers = {
           index: "dashboard_cells",
           body: query
         });
+        console.log(results)
         return results["aggregations"]["agg_terms_cell_type"]["buckets"] //results["hits"]["hits"].map(record => record["_source"]);
       }
     },
